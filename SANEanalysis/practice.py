a029a1e2fedcde899da43114a93eb0fd76c055bd
Ahana Sen <ahana.sen@duke.edu>
2020-05-06 23:47:56 -0700

    made first df

--- SANEanalysis/practice.py
+++ SANEanalysis/practice.py
@@ -1,6 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from pprint import pprint
+import pandas as pd
 
 client_id = "74d2b05f4e4c490daaa4b7d1fb0a2177"
 client_secret = "865dd84e8dc04935a6d181dbf764889b"
@@ -8,14 +8,49 @@ client_credentials_manager = SpotifyClientCredentials(client_id=client_id, clien
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 pl_id = 'spotify:playlist:0ThF3wyjhsWQPItf2s7M3V'
-offset = 0
 
-while True:
-    response = sp.playlist_tracks(pl_id,
-                                  offset=offset,
-                                  fields='items.track.id,total')
-    pprint(response['items'])
-    if len(response['items']) == 0:
-        break
-        x=2
-        y=2
+
+response = sp.playlist_tracks(pl_id)
+
+
+exdf = {}
+exdf["adderID"] = []
+exdf['danceability'] = []
+exdf['energy'] = []
+exdf['loudness'] = []
+exdf['speechiness'] = []
+exdf['instrumentalness'] = []
+exdf['tempo'] =[]
+exdf['duration_ms'] = []
+exdf['songname'] = []
+exdf['artistname'] = []
+exdf['popularity'] = []
+exdf['genre'] = []
+exdf['releasedate'] = []
+
+
+
+for item in response['items']:
+    exdf["adderID"].append(item['added_by']['id'])
+    tempfeat = sp.audio_features(item['track']['uri'])
+    exdf["danceability"].append(tempfeat[0]['danceability'])
+    exdf["energy"].append(tempfeat[0]['energy'])
+    exdf["loudness"].append(tempfeat[0]['loudness'])
+    exdf["speechiness"].append(tempfeat[0]['speechiness'])
+    exdf["instrumentalness"].append(tempfeat[0]['instrumentalness'])
+    exdf["tempo"].append(tempfeat[0]['tempo'])
+    exdf["duration_ms"].append(tempfeat[0]['duration_ms'])
+    name = item['track']['artists'][0]['name']
+    exdf["artistname"].append(name)
+    song = item['track']['name']
+    exdf["songname"].append(song)
+    popu = item['track']['popularity']
+    exdf["popularity"].append(popu)
+    reldate = item['track']['album']['release_date']
+    exdf['releasedate'].append(reldate)
+    genres = sp.artist(item['track']['artists'][0]['uri'])
+    exdf['genre'].append(genres)
+
+acdf = pd.DataFrame.from_dict(exdf)
+
+acdf.to_csv("test.csv")
